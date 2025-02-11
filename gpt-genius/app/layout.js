6fcddfe8c76ac31378299c39a3c715e3b89c9436
Dennis <dmarlo96@live.ca>
2025-02-11 17:06:20 -0500

    Toggle themes

--- gpt-genius/app/layout.js
+++ gpt-genius/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<ClerkProvider>
-			<html data-theme='autumn' lang='en'>
+			<html lang='en'>
 				<body className={inter.className}>{children}</body>
 			</html>
 		</ClerkProvider>
--- gpt-genius/components/ThemeToggle.jsx
+++ gpt-genius/components/ThemeToggle.jsx
@@ -1,10 +1,34 @@
+'use client';
+
 import React from 'react';
-import { FaMoon } from 'react-icons/fa';
+import { useState } from 'react';
+import { BsMoonFill, BsSunFill } from 'react-icons/bs';
+
+const themes = {
+	autumn: 'autumn',
+	business: 'business'
+};
 
 const ThemeToggle = () => {
+	const [theme, setTheme] = useState(themes.autumn);
+
+	const toggleTheme = () => {
+		// toggle between autumn and business theme (light/dark mode)
+		const newTheme =
+			theme === themes.autumn ? themes.business : themes.autumn;
+
+		// Set the theme by adding a data-theme attribute to <html> for CSS to detect
+		document.documentElement.setAttribute('data-theme', newTheme);
+		setTheme(newTheme);
+	};
+
 	return (
-		<button className='btn btn-primary btn-sm'>
-			<FaMoon />
+		<button onClick={toggleTheme} className='btn btn-primary btn-sm'>
+			{theme === 'autumn' ? (
+				<BsMoonFill className='h-4 w-4 ' title='Dark Mode' />
+			) : (
+				<BsSunFill className='h-4 w-4' title='Light Mode' />
+			)}
 		</button>
 	);
 };
