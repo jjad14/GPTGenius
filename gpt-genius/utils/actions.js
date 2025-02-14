5d217e36c62b2d7b881d8c7be131b259625a6014
Dennis <dmarlo96@live.ca>
2025-02-14 13:16:50 -0500

    Initial OpenAI setup

--- gpt-genius/utils/actions.js
+++ gpt-genius/utils/actions.js
@@ -1,6 +1,23 @@
 'use server';
 
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY
+});
+
 export const generateChatResponse = async (chatMessage) => {
-	console.log('chatMessage:' + chatMessage);
+	const response = await openai.chat.completions.create({
+		messages: [
+			{ role: 'system', content: 'You are a helpful assistant.' }, // Were we set what the model should be
+			{ role: 'user', content: chatMessage } // The message from the user
+		],
+		model: 'gpt-4o-mini',
+		temperature: 0
+	});
+
+	console.log(response.choices[0].message);
+	console.log(response);
+
 	return 'Test Response';
 };
