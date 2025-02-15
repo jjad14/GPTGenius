543b1845264dc1f8c5ca33abac1c724c4381ff32
Dennis <dmarlo96@live.ca>
2025-02-15 18:56:58 -0500

    Added Tour Search Functionality

--- gpt-genius/components/ToursPage.jsx
+++ gpt-genius/components/ToursPage.jsx
@@ -1,18 +1,40 @@
 'use client';
 
 import React from 'react';
-import { getAllTours } from '@/utils/actions';
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import { getAllTours } from '@/utils/actions';
 import ToursList from './ToursList';
 
 const ToursPage = () => {
+	const [searchValue, setSearchValue] = useState('');
 	const { data, isPending } = useQuery({
-		queryKey: ['tours'],
-		queryFn: () => getAllTours()
+		queryKey: ['tours', searchValue],
+		queryFn: () => getAllTours(searchValue)
 	});
 
 	return (
 		<>
+			<form className='max-w-lg mb-12'>
+				<div className='join w-full'>
+					<input
+						type='text'
+						placeholder='Search for tour(s)'
+						className='input input-bordered join-item w-full'
+						name='search'
+						value={searchValue}
+						onChange={(e) => setSearchValue(e.target.value)}
+						required
+					/>
+					<button
+						className='btn btn-primary join-item'
+						type='button'
+						disabled={isPending}
+						onClick={() => setSearchValue('')}>
+						{isPending ? 'Loading...' : 'Clear'}
+					</button>
+				</div>
+			</form>
 			{isPending ? (
 				<span className=' loading'></span>
 			) : (
