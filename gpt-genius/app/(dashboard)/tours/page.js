1d03bf6cd458c69489cc924cbcfa304001fb97ae
Dennis <dmarlo96@live.ca>
2025-02-15 18:03:12 -0500

    Tours Page Setup

--- gpt-genius/app/(dashboard)/tours/page.js
+++ gpt-genius/app/(dashboard)/tours/page.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import ToursPage from '@/components/ToursPage';
+import {
+	dehydrate,
+	HydrationBoundary,
+	QueryClient
+} from '@tanstack/react-query';
+import { getAllTours } from '@/utils/actions';
 
-const ToursPage = () => {
-	return <div>ToursPage</div>;
+const AllToursPage = async () => {
+	const queryClient = new QueryClient();
+
+	await queryClient.prefetchQuery({
+		queryKey: ['tours'],
+		queryFn: () => getAllTours()
+	});
+
+	return (
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<ToursPage />
+		</HydrationBoundary>
+	);
 };
 
-export default ToursPage;
+export default AllToursPage;
