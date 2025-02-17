c456452c20477c14298443e38199db14328f2b67
Dennis <dmarlo96@live.ca>
2025-02-16 19:29:47 -0500

    Display Token amount

--- gpt-genius/components/MemberProfile.jsx
+++ gpt-genius/components/MemberProfile.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { UserButton, currentUser, auth } from '@clerk/nextjs';
+import { fetchOrGenerateTokens } from '@/utils/actions';
 
 const MemberProfile = async () => {
 	const user = await currentUser(); // currently authenticated user
 	const { userId } = auth(); // authentication context
+
+	await fetchOrGenerateTokens(userId);
 
 	return (
 		<div className='px-4 flex items-center gap-2'>
