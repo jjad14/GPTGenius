939a281417554a95362a83f1d74a6b978e4648a7
Dennis <dmarlo96@live.ca>
2025-02-16 19:11:57 -0500

    Added Token Model and Token Actions

--- gpt-genius/utils/actions.js
+++ gpt-genius/utils/actions.js
@@ -144,6 +144,49 @@ export const generateTourImage = async ({ city, country }) => {
 	}
 };
 
+export const fetchUserTokensById = async (clerkId) => {
+	const result = await prisma.token.findUnique({
+		where: {
+			clerkId
+		}
+	});
+
+	return result?.tokens;
+};
+
+export const generateUserTokensForId = async (clerkId) => {
+	const result = await prisma.token.create({
+		data: {
+			clerkId
+		}
+	});
+	return result?.tokens;
+};
+
+export const fetchOrGenerateTokens = async (clerkId) => {
+	const result = await fetchUserTokensById(clerkId);
+	if (result) {
+		return result.tokens;
+	}
+	return (await generateUserTokensForId(clerkId)).tokens;
+};
+
+export const subtractTokens = async (clerkId, tokens) => {
+	const result = await prisma.token.update({
+		where: {
+			clerkId
+		},
+		data: {
+			tokens: {
+				decrement: tokens
+			}
+		}
+	});
+	revalidatePath('/profile');
+	// Return the new token value
+	return result.tokens;
+};
+
 export const toProperCase = (str) => {
 	return str
 		.split(' ')
